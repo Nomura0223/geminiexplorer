a9bcbae988acb8c703046673f437cbc2f2b60c81
Nomura0223 <ysnr.nmr@gmail.com>
2024-01-08 12:18:25 +0900

    update_app.py

--- app.py
+++ app.py
@@ -13,24 +13,36 @@ st.sidebar.title('設定', anchor='sidebar')
 st.sidebar.subheader('', divider="rainbow")
 
 language = st.sidebar.selectbox('言語:',
-                list(["日本語", "英語"]))
+                list(["日本語", "English"]))
 
-formarity = st.sidebar.selectbox('シーン:', 
-             list(["ビジネス", "プライベート"]))
+receiver = st.sidebar.selectbox('受信者:',
+                list(["顧客", "上司", "同僚", "その他"]))
+if receiver == "その他":
+    receiver = st.sidebar.text_input('その他:')
 
-perpose = st.sidebar.selectbox('目的:', 
-             list(["打合せの日程調整", "その他"]))
+formarity = st.sidebar.selectbox('トーン:', 
+             list(["フォーマル", "カジュアル"]))
 
-if perpose == "その他":
-    perpose = st.sidebar.text_input('その他:')
+perpose = st.sidebar.text_input('目的:', placeholder="会議の招待、進捗報告など", value="")
 
-length = st.sidebar.slider('文字数:', 100, 500, 300)
+other = st.sidebar.text_input('(任意) その他情報:', placeholder="明日までに返信が欲しい", value="")
 
-st.sidebar.subheader('', divider="rainbow")
+# perpose = st.sidebar.selectbox('目的:', 
+#              list(["会議の招待", "進捗報告", "その他"]))
 
-reply = st.sidebar.toggle('返信')
-if reply:
-    receivemail = st.sidebar.text_area('受信メール:', height=400)
+# if perpose == "その他":
+#     perpose = st.sidebar.text_input('その他:')
+
+
+attachment = st.sidebar.toggle('添付ファイル')
+if attachment:
+    attachmentFile = st.sidebar.text_input('ファイルの種類', placeholder="進捗報告資料など", value="")
+
+length = st.sidebar.slider('文字数:', min_value=100, max_value=500,value=300, step=50)
+
+# reply = st.sidebar.toggle('返信')
+# if reply:
+#     receivemail = st.sidebar.text_area('受信メール:', height=400)
 
 st.sidebar.subheader('', divider="rainbow")
 
@@ -50,18 +62,24 @@ with col2:
 st.subheader('', divider="rainbow")
 
 user_input = ""
-user_input += f"言語は{language}で文章を作成してください。{length}文字程度の{formarity}シーンでのメールの文章作成してください。メールの目的は{perpose}です。"
+user_input += f"回答言語は「{language}」を使用。次の内容を考慮してメールを作成ください。受信者は「{receiver}」、目的は「{perpose}」です。文字数は「{length}」程度で「{formarity}」なトーンでお願いします。"
 
-if reply:
-    user_input += f"文章は次の受信メールに対する返信として作成下さい。\n\n{receivemail}"
+if other != "":
+    user_input += f"メールには「{other}」ことを記載ください。"
+
+if attachment:
+    user_input += f"メール末尾には「{attachmentFile}」を添付します。"
 
 # ボタンが押されたら
 if button:
-    user_text = st.chat_message("user")
-    user_text.write(user_input)
-    with st.spinner('文章を作成中...'):
-        response = model.generate_content(user_input)
-    message = st.chat_message("ai")
-    message.write(response.text)
-    st.subheader('', divider="rainbow")
-
+    if perpose != "":
+        user_text = st.chat_message("user")
+        user_text.write(user_input)
+        with st.spinner('文章を作成中...'):
+            response = model.generate_content(user_input)
+        message = st.chat_message("ai")
+        message.write(response.text)
+        st.subheader('', divider="rainbow")
+    else:
+        st.error('目的を入力してください。')
+    
